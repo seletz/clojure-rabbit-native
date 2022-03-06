97ef29f58d064c81722ea45b39e5bc216236720e
Stefan Eletzhofer <stefan.eletzhofer@nexiles.com>
2022-03-06 20:35:54 +0100

    Configure logging correctly.

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject rmq-tool "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.slf4j/slf4j-api "1.7.30"]
+                 [com.fzakaria/slf4j-timbre "0.3.21"]
+                 [com.taoensso/timbre "5.1.2"]
                  [com.novemberain/langohr "5.1.0"]]
 
   :plugins [[io.taylorwood/lein-native-image "0.3.1"]]    ;; or in ~/.lein/profiles.clj
--- src/rmq_tool/core.clj
+++ src/rmq_tool/core.clj
@@ -5,14 +5,19 @@
             [langohr.channel   :as lch]
             [langohr.queue     :as lq]
             [langohr.consumers :as lc]
-            [langohr.basic     :as lb]))
+            [langohr.basic     :as lb]
+            [taoensso.timbre :as timbre
+             :refer [log  trace  debug  info  warn  error  fatal  report
+                     logf tracef debugf infof warnf errorf fatalf reportf
+                     spy get-env]]
+            ))
 
 (def ^{:const true}
   default-exchange-name "")
 
 (defn message-handler
   [ch {:keys [content-type delivery-tag type] :as meta} ^bytes payload]
-  (println (format "[consumer] Received a message: %s, delivery tag: %d, content type: %s, type: %s"
+  (info (format "[consumer] Received a message: %s, delivery tag: %d, content type: %s, type: %s"
                    (String. payload "UTF-8") delivery-tag content-type type)))
 
 (defn -main
@@ -20,13 +25,12 @@
   (let [conn  (rmq/connect)
         ch    (lch/open conn)
         qname "langohr.examples.hello-world"]
-    (println (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
+    (info (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
     (lq/declare ch qname {:exclusive false :auto-delete true})
     (lc/subscribe ch qname message-handler {:auto-ack true})
     (lb/publish ch default-exchange-name qname "Hello!" {:content-type "text/plain" :type "greetings.hi"})
     (Thread/sleep 2000)
-    (println "[main] Disconnecting...")
+    (info "[main] Disconnecting...")
     (rmq/close ch)
     (rmq/close conn)))
 
-(println "Loadmeup")
